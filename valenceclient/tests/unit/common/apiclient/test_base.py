82fbe7e0dcb19c3cbdafe80881fb5c9a23452b32
jinxingfang <fang.jinxing@99cloud.net>
2017-02-10 14:39:17 +0800

    Add abstract http method process
    Add abstrace http method, such as POST, PUT, DELETE, GET.
    Add some common execption and some unittest.
    
    Change-Id: I65016ea2cba022823a80b044fc2e17801535a88b

---
+++ valenceclient/tests/unit/common/apiclient/test_base.py
@@ -0,0 +1,26 @@
+from oslotest import base as test_base
+from valenceclient.common.apiclient import base
+
+class CrudResource(base.Resource):
+    pass
+
+class CrudResourceManager(base.CrudManager):
+    """Manager class for manipulating Identity crud_resources."""
+    resource_class = CrudResource
+    collection_key = 'crud_resources'
+    key = 'crud_resource'
+
+    def get(self, crud_resource):
+        return super(CrudResourceManager, self).get(
+            crud_resource_id=base.getid(crud_resource))
+
+
+class ResourceTest(test_base.BaseTestCase):
+    def test_reource_repr(self):
+        r = base.Resource(None, dict(foo='bar', baz='spam'))
+        self.assertEqual('<Resource baz=spam, foo=bar>', repr(r))
+
+    def test_getid(self):
+        class TmpObject(base.Resource):
+            id = '4'
+        self.assertEqual('4', base.getid(TmpObject(None, {})))
