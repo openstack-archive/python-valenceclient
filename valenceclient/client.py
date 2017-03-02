8ef875f971f86f7ac6fd31a7c0a4510b5694fe02
jinxingfang <fang.jinxing@99cloud.net>
2017-03-02 15:30:32 +0800

    Add Client for access api
    Get a client based on the valence server url.
    
    Change-Id: I29a4a27daae99fccfacd53b68951d4c786418ca7

---
+++ valenceclient/client.py
@@ -0,0 +1,38 @@
+# Copyright 2016 99cloud
+# All Rights Reserved.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+from oslo_utils import importutils
+from valenceclient.common.http import DEFAULT_VERSION
+from valenceclient import exc
+
+
+def get_client(valence_url, valence_api_version=None, max_retries=None,
+        retry_interval=None):
+
+    if valence_url is None:
+        raise exc.InvalidValenceUrl
+    valence_api_version = valence_api_version if valence_api_version \
+                          else DEFAULT_VERSION
+
+    kwargs = {
+        'valence_url': valence_url,
+        'max_retries': max_retries,
+        'retry_interval': retry_interval
+    }
+    module = importutils.import_versioned_module('valenceclient',
+                                                 DEFAULT_VERSION,
+                                                 'client')
+    client_class = getattr(module, 'Client')
+    return client_class(**kwargs)
