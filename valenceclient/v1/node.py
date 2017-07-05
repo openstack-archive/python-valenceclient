a0ee3001dd520e0fb7a0f2ea19d4609a9ac62fb4
jinxingfang <fang.jinxing@99cloud.net>
2017-07-05 12:14:50 +0800

    Add CLI node operation
    Add CLI node operation:
    
    compose node: support args: --memory, --cpu, --storage
    list all nodes information
    delete node by id
    get node detail information by id
    
    Change-Id: I81d57e08df108ee11c192a0a4151c6d6259eb16d

--- valenceclient/v1/node.py
+++ valenceclient/v1/node.py
@@ -1,5 +1,21 @@
+# Copyright 2017 99cloud, Inc.
+# All Rights Reserved.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
 
 from valenceclient.common import base
+
 
 class Node(base.Resource):
     def __repr__(self):
@@ -12,8 +28,8 @@ class NodeManager(base.CreateManager):
     _creation_attributes = ['Processors', 'RemoteDrives', 'Memory']
 
     def list(self, resource_class=None):
-
-        nodes = self._list(self._path(),resource_class if resource_class else self.resource_class)
+        nodes = self._list(self._path(),
+                resource_class if resource_class else self.resource_class)
         return nodes
 
     def get(self, node_id):
@@ -27,7 +43,3 @@ class NodeManager(base.CreateManager):
                             method=http_method)
     def compose(self, **kwargs):
         return  self.create(**kwargs)
-
-
-
-
