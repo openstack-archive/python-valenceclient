ca633d3af6ee5ce8251f06237bcce0b8bdfa6c12
Anusha Ramineni <anusha.ramineni@nectechnologies.in>
2017-08-23 15:57:20 +0530

    Add command to list pod managers
    'openstack valence podmanager list' is added to list the podmanagers
    created by valence.
    
    Change-Id: Ifc9df95c525383abfcc85deaaf6b275ed82faa86

---
+++ valenceclient/osc/v1/podmanager.py
@@ -0,0 +1,33 @@
+#   Copyright 2017 Intel, Inc.
+#
+#   Licensed under the Apache License, Version 2.0 (the "License"); you may
+#   not use this file except in compliance with the License. You may obtain
+#   a copy of the License at
+#
+#        http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#   WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#   License for the specific language governing permissions and limitations
+#   under the License.
+#
+
+from osc_lib.command import command
+from osc_lib import utils
+
+
+class ListPodManagers(command.Lister):
+    _description = "Lists all registered podmanagers"
+    auth_required = False
+
+    def get_parser(self, prog_name):
+        parser = super(ListPodManagers, self).get_parser(prog_name)
+        return parser
+
+    def take_action(self, parsed_args):
+        self.log.debug("take_action(%s)", parsed_args)
+        client = self.app.client_manager.valence
+        obj = client.list_podmanagers()
+        columns = ['uuid', 'name', 'url', 'status', 'created_at', 'updated_at']
+        return (columns, (utils.get_dict_properties(s, columns) for s in obj))
